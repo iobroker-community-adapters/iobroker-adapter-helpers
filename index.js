81eb413de60ed8f2b5de4c3ef5b53b82b558bd94
jey-cee <jey-cee@live.com>
2020-04-18 23:21:52 +0200

    add object Attributes list add roles list

---
+++ index.js
@@ -0,0 +1,4 @@
+const objAttributes = require('/definitions/object_attributes.js');
+const roles = require('/definitions/roles.js').roles_definitions;
+
+module.exports = {objAttributes: objAttributes, roles: roles};
