a3c31d4caa04e5f758e34a9c8f3a62733c033395
jey-cee <jey-cee@live.com>
2020-04-20 10:54:30 +0200

    add units

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const objAttributes = require('/definitions/object_attributes.js');
 const roles = require('/definitions/roles.js').roles_definitions;
-
-module.exports = {objAttributes: objAttributes, roles: roles};
+const units = require('/definitions/units.js').units;
+module.exports = {objAttributes: objAttributes, roles: roles, units: units};
