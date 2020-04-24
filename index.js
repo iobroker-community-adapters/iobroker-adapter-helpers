286d128446ed0f0dff3551e94055c55fd60cc0bf
jey-cee <jey-cee@live.com>
2020-04-24 20:22:17 +0200

    version update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const objAttributes = require('/definitions/object_attributes.js');
 const roles = require('/definitions/roles.js').roles_definitions;
 const units = require('/definitions/units.js').units;
+
 module.exports = {objAttributes: objAttributes, roles: roles, units: units};
