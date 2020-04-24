fe710c63f63ec39de8b2afa70d9cef65db766cbe
Dutchman <DutchmanNL@users.noreply.github.com>
2020-04-24 20:38:50 +0200

    fix file location

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const objAttributes = require('/definitions/object_attributes.js');
-const roles = require('/definitions/roles.js');
-const units = require('/definitions/units.js');
+const objAttributes = require('./definitions/object_attributes.js');
+const roles = require('./definitions/roles.js');
+const units = require('./definitions/units.js');
 
 module.exports = {objAttributes: objAttributes, roles: roles, units: units};
