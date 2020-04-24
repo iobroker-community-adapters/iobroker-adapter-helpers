71b753eea62ccc6cafbaa41ecacda98c4d292bae
jey-cee <jey-cee@live.com>
2020-04-24 21:26:39 +0200

    direct export of consts

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const objAttributes = require('./definitions/object_attributes.js');
-const roles = require('./definitions/roles.js');
-const units = require('./definitions/units.js');
+const commonAttributes = require('./definitions/object_attributes.js').commonAttributes;
+const objectTypes = require('./definitions/object_attributes.js').objectTypes ;
+const roles = require('./definitions/roles.js').roles;
+const units = require('./definitions/units.js').units;
 
 module.exports = {objAttributes: objAttributes, roles: roles, units: units};
