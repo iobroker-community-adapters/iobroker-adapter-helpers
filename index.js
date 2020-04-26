17b7959d2bb4a3f264383e9072b616e97ae8d4e3
DutchmanNL <rdrozda86@gmail.com>
2020-04-26 07:46:06 +0200

    ### 0.0.7 shorten unit and fix definitions export

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const objAttributes = require('./definitions/object_attributes.js');
 const roles = require('./definitions/roles.js');
-const units = require('./definitions/units.js');
+const units = require('./definitions/units.js').units;
 
 module.exports = {objAttributes: objAttributes, roles: roles, units: units};
